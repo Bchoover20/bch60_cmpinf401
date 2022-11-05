29a7f3e89c40e0f05da780864682395ec0147211
Bchoover20 <112009591+Bchoover20@users.noreply.github.com>
2022-11-04 22:12:50 -0400

    Assignment 2 completed and ready to submit

--- bch60_MenuManager_v2/src/bch60_MenuManager/FileManager.java
+++ bch60_MenuManager_v2/src/bch60_MenuManager/FileManager.java
@@ -1,297 +1,285 @@
 package bch60_MenuManager;
 
-	import java.io.BufferedReader;
-	import java.io.FileReader;
-    import java.io.IOException;
-    import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    /**
-     * Class: Menu
-     * @author Brandon Hoover
-     * Created: 10/25/2022
-     */
+/**
+ * Class: Menu
+ * @author Brandon Hoover
+ * Created: 10/25/2022
+ */
 
-    public class FileManager {
+public class FileManager {
 
+	/**
+	 * Method readEntrees
+	 * @param String fileName - relative file path that path should be set too
+	 * @return entreeList - Array list of type Entree that contains the possible list of entrees
+	 * that could be used on the menu from entrees.txt
+	 */
 
-    	public static ArrayList<Entree> readEntrees(String fileName) {
-    		String path = fileName;
-    		// Absolute path -- C:\Users\Brandon\Documents\GitHub\bch60_cmpinf401\bch60_MenuManager_v2\data 
-    		// Creating an array list to throw in Entree data that fits type entree parameter (name, desc, cal)
-    		ArrayList<Entree> entreeList  = new ArrayList<Entree>();
+	public static ArrayList<Entree> readEntrees(String fileName) {
+		String path = fileName;
+		// Absolute path -- C:\Users\Brandon\Documents\GitHub\bch60_cmpinf401\bch60_MenuManager_v2\data 
+		// Creating an array list to throw in Entree data that fits type entree parameter (name, desc, cal)
+		ArrayList<Entree> entreeList  = new ArrayList<Entree>();
 
-    		try {
+		try {
 
-    			FileReader fr = new FileReader(path);
-    			BufferedReader br = new BufferedReader(fr); 
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr); 
 
-    			// Initializing the state of line
-    			String line = null;
+			// Initializing the state of line
+			String line = null;
 
-    			int counter=0;
-    			ArrayList<String> dataCollector = new ArrayList<String>();
-    			// Reading through each line until its null
-    			while ((line = br.readLine()) != null) {
+			int counter=0;
+			ArrayList<String> dataCollector = new ArrayList<String>();
+			// Reading through each line until its null
+			while ((line = br.readLine()) != null) {
 
-    				String[] tempLine = line.split("@@");
+				String[] tempLine = line.split("@@");
 
-    				dataCollector.add(tempLine[0]);
-    				dataCollector.add(tempLine[1]);
-    				dataCollector.add(tempLine[2]);
+				dataCollector.add(tempLine[0]);
+				dataCollector.add(tempLine[1]);
+				dataCollector.add(tempLine[2]);
 
-    				counter++;	
-    			}
-    			
-    			int counter2=1;
-    			int i=0,j=1,k=2;
-    			int tempCal=0;
+				counter++;	
+			}
 
-    			while (counter2 <= counter) {
+			int counter2=1;
+			int i=0,j=1,k=2;
+			int tempCal=0;
 
-    				tempCal = Integer.parseInt(dataCollector.get(k));
+			while (counter2 <= counter) {
 
-    				Entree testEntree = new Entree(dataCollector.get(i), dataCollector.get(j), tempCal);
+				tempCal = Integer.parseInt(dataCollector.get(k));
 
-    				entreeList.add(testEntree);
+				Entree testEntree = new Entree(dataCollector.get(i), dataCollector.get(j), tempCal);
 
-    				i = (i + 3);
-    				j = (j + 3);
-    				k = (k + 3);
+				entreeList.add(testEntree);
 
-    				counter2++;
-    			}
-    			fr.close();
-    			br.close();
-    		}
-    		catch (IOException e){
-    			System.out.println("IT HAPPENED IN ARRAY LIST ENTREE");
-    			e.printStackTrace();
-    		}
-    		return entreeList;
+				i = (i + 3);
+				j = (j + 3);
+				k = (k + 3);
 
-    	}
-    	
-    	public static ArrayList<Side> readSides(String fileName) {
-    		String path = fileName;
-    		// Absolute path -- C:\Users\Brandon\Documents\GitHub\bch60_cmpinf401\bch60_MenuManager_v2\data 
-    		// Creating an array list to throw in Entree data that fits type entree parameter (name, desc, cal)
-    		ArrayList<Side> sideList  = new ArrayList<Side>();
+				counter2++;
+			}
+			fr.close();
+			br.close();
+		}
+		catch (IOException e){
+			System.out.println("IT HAPPENED IN ARRAY LIST ENTREE");
+			e.printStackTrace();
+		}
+		return entreeList;
 
-    		try {
+	}
 
-    			FileReader fr = new FileReader(path);
-    			BufferedReader br = new BufferedReader(fr); 
+	/**
+	 * Method readSides
+	 * @param String fileName - relative file path that path should be set too
+	 * @return sideList - Array list of type Side that contains the possible list of sides
+	 * that could be used on the menu from sides.txt
+	 */
 
-    			// Initializing the state of line
-    			String line = null;
+	public static ArrayList<Side> readSides(String fileName) {
+		String path = fileName;
+		ArrayList<Side> sideList  = new ArrayList<Side>();
 
-    			int counter=0;
-    			ArrayList<String> dataCollector = new ArrayList<String>();
-    			// Reading through each line until its null
-    			while ((line = br.readLine()) != null) {
+		try {
 
-    				String[] tempLine = line.split("@@");
-    				
-    				// Verification Check
-    				/*System.out.println(tempLine[0]);
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr); 
+
+			// Initializing the state of line
+			String line = null;
+
+			int counter=0;
+			ArrayList<String> dataCollector = new ArrayList<String>();
+			// Reading through each line until its null
+			while ((line = br.readLine()) != null) {
+
+				String[] tempLine = line.split("@@");
+
+				// Verification Check
+				/*System.out.println(tempLine[0]);
     				System.out.println(tempLine[1]);
     				System.out.println(tempLine[2]);
-    				*/
+				 */
 
-    				dataCollector.add(tempLine[0]);
-    				dataCollector.add(tempLine[1]);
-    				dataCollector.add(tempLine[2]);
+				dataCollector.add(tempLine[0]);
+				dataCollector.add(tempLine[1]);
+				dataCollector.add(tempLine[2]);
 
-    				counter++;	
-    			}
-    			
-    			int counter2=1;
-    			int i=0,j=1,k=2;
-    			int tempCal=0;
+				counter++;	
+			}
 
-    			while (counter2 <= counter) {
+			int counter2=1;
+			int i=0,j=1,k=2;
+			int tempCal=0;
 
-    				tempCal = Integer.parseInt(dataCollector.get(k));
+			while (counter2 <= counter) {
 
-    				Side addSide = new Side(dataCollector.get(i), dataCollector.get(j), tempCal);
+				tempCal = Integer.parseInt(dataCollector.get(k));
 
-    				sideList.add(addSide);
+				Side addSide = new Side(dataCollector.get(i), dataCollector.get(j), tempCal);
 
-    				i = (i + 3);
-    				j = (j + 3);
-    				k = (k + 3);
+				sideList.add(addSide);
 
-    				counter2++;
-    			}
-    			fr.close();
-    			br.close();
-    		}
-    		catch (IOException e){
-    			System.out.println("The error occurred in readSides");
-    			e.printStackTrace();
-    		}
-    		return sideList;
-    		
-    	}
-    	
-    	public static ArrayList<Salad> readSalads(String fileName) {
-    		String path = fileName;
-    		// Absolute path -- C:\Users\Brandon\Documents\GitHub\bch60_cmpinf401\bch60_MenuManager_v2\data 
-    		// Creating an array list to throw in Entree data that fits type entree parameter (name, desc, cal)
-    		ArrayList<Salad> saladList  = new ArrayList<Salad>();
+				i = (i + 3);
+				j = (j + 3);
+				k = (k + 3);
 
-    		try {
+				counter2++;
+			}
+			fr.close();
+			br.close();
+		}
+		catch (IOException e){
+			System.out.println("The error occurred in readSides");
+			e.printStackTrace();
+		}
+		return sideList;
 
-    			FileReader fr = new FileReader(path);
-    			BufferedReader br = new BufferedReader(fr); 
+	}
 
-    			// Initializing the state of line
-    			String line = null;
+	/**
+	 * Method readSalads
+	 * @param String fileName - relative file path that path should be set too
+	 * @return saladList - Array list of type Salad that contains the possible list of Salads
+	 * that could be used on the menu from salads.txt
+	 */
 
-    			int counter=0;
-    			ArrayList<String> dataCollector = new ArrayList<String>();
-    			// Reading through each line until its null
-    			while ((line = br.readLine()) != null) {
+	public static ArrayList<Salad> readSalads(String fileName) {
+		String path = fileName;
+		ArrayList<Salad> saladList  = new ArrayList<Salad>();
 
-    				String[] tempLine = line.split("@@");
-    				
-    				// Verification Check
-    				/*System.out.println(tempLine[0]);
+		try {
+
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr); 
+
+			// Initializing the state of line
+			String line = null;
+
+			int counter=0;
+			ArrayList<String> dataCollector = new ArrayList<String>();
+			// Reading through each line until its null
+			while ((line = br.readLine()) != null) {
+
+				String[] tempLine = line.split("@@");
+
+				// Verification Check
+				/*System.out.println(tempLine[0]);
     				System.out.println(tempLine[1]);
     				System.out.println(tempLine[2]);
-    				*/
+				 */
 
-    				dataCollector.add(tempLine[0]);
-    				dataCollector.add(tempLine[1]);
-    				dataCollector.add(tempLine[2]);
+				dataCollector.add(tempLine[0]);
+				dataCollector.add(tempLine[1]);
+				dataCollector.add(tempLine[2]);
 
-    				counter++;	
-    			}
-    			
-    			int counter2=1;
-    			int i=0,j=1,k=2;
-    			int tempCal=0;
+				counter++;	
+			}
 
-    			while (counter2 <= counter) {
+			int counter2=1;
+			int i=0,j=1,k=2;
+			int tempCal=0;
 
-    				tempCal = Integer.parseInt(dataCollector.get(k));
+			while (counter2 <= counter) {
 
-    				Salad addSalad = new Salad(dataCollector.get(i), dataCollector.get(j), tempCal);
+				tempCal = Integer.parseInt(dataCollector.get(k));
 
-    				saladList.add(addSalad);
+				Salad addSalad = new Salad(dataCollector.get(i), dataCollector.get(j), tempCal);
 
-    				i = (i + 3);
-    				j = (j + 3);
-    				k = (k + 3);
+				saladList.add(addSalad);
 
-    				counter2++;
-    			}
-    			fr.close();
-    			br.close();
-    		}
-    		catch (IOException e){
-    			System.out.println("The error occurred in readSalads");
-    			e.printStackTrace();
-    		}
-    		return saladList;
-    		
-    	}
-    	
-    	public static ArrayList<Dessert> readDesserts(String fileName) {
-    		String path = fileName;
-    		// Absolute path -- C:\Users\Brandon\Documents\GitHub\bch60_cmpinf401\bch60_MenuManager_v2\data 
-    		// Creating an array list to throw in Entree data that fits type entree parameter (name, desc, cal)
-    		ArrayList<Dessert> dessertList  = new ArrayList<Dessert>();
+				i = (i + 3);
+				j = (j + 3);
+				k = (k + 3);
 
-    		try {
+				counter2++;
+			}
+			fr.close();
+			br.close();
+		}
+		catch (IOException e){
+			System.out.println("The error occurred in readSalads");
+			e.printStackTrace();
+		}
+		return saladList;
 
-    			FileReader fr = new FileReader(path);
-    			BufferedReader br = new BufferedReader(fr); 
+	}
 
-    			// Initializing the state of line
-    			String line = null;
+	/**
+	 * Method readDesserts
+	 * @param String fileName - relative file path that path should be set too
+	 * @return dessertList - Array list of type Dessert that contains the possible list of desserts
+	 * that could be used on the menu from desserts.txt
+	 */
 
-    			int counter=0;
-    			ArrayList<String> dataCollector = new ArrayList<String>();
-    			// Reading through each line until its null
-    			while ((line = br.readLine()) != null) {
+	public static ArrayList<Dessert> readDesserts(String fileName) {
+		String path = fileName;
+		ArrayList<Dessert> dessertList  = new ArrayList<Dessert>();
 
-    				String[] tempLine = line.split("@@");
-    				
-    				// Verification Check
-    				/*System.out.println(tempLine[0]);
+		try {
+
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr); 
+
+			// Initializing the state of line
+			String line = null;
+
+			int counter=0;
+			ArrayList<String> dataCollector = new ArrayList<String>();
+			// Reading through each line until its null
+			while ((line = br.readLine()) != null) {
+
+				String[] tempLine = line.split("@@");
+
+				// Verification Check
+				/*System.out.println(tempLine[0]);
     				System.out.println(tempLine[1]);
     				System.out.println(tempLine[2]);
-    				*/
+				 */
 
-    				dataCollector.add(tempLine[0]);
-    				dataCollector.add(tempLine[1]);
-    				dataCollector.add(tempLine[2]);
+				dataCollector.add(tempLine[0]);
+				dataCollector.add(tempLine[1]);
+				dataCollector.add(tempLine[2]);
 
-    				counter++;	
-    			}
-    			
-    			int counter2=1;
-    			int i=0,j=1,k=2;
-    			int tempCal=0;
+				counter++;	
+			}
 
-    			while (counter2 <= counter) {
+			int counter2=1;
+			int i=0,j=1,k=2;
+			int tempCal=0;
 
-    				tempCal = Integer.parseInt(dataCollector.get(k));
+			while (counter2 <= counter) {
 
-    				Dessert addDessert = new Dessert(dataCollector.get(i), dataCollector.get(j), tempCal);
+				tempCal = Integer.parseInt(dataCollector.get(k));
 
-    				dessertList.add(addDessert);
+				Dessert addDessert = new Dessert(dataCollector.get(i), dataCollector.get(j), tempCal);
 
-    				i = (i + 3);
-    				j = (j + 3);
-    				k = (k + 3);
+				dessertList.add(addDessert);
 
-    				counter2++;
-    			}
-    			fr.close();
-    			br.close();
-    		}
-    		catch (IOException e){
-    			System.out.println("The error occurred in readDesserts");
-    			e.printStackTrace();
-    		}
-    		return dessertList;
-    		
-    	}
+				i = (i + 3);
+				j = (j + 3);
+				k = (k + 3);
 
-   
-	 /*try {
-		 BufferedReader br = new BufferedReader(new FileReader("data/entree.txt")); 
-	 
-		String line = br.readLine();
-		String[] array = line.split("@@");
-	 }
-	 catch (IOException e ) {
-		 System.out.println("Incorrect Input!");
-		 e.printStackTrace();
-	 }
-	 
-	 		 /*
-		 int counter3=0;
-		 
-		 for (String element : dataSearcher) {
-			 
-			 System.out.println(dataSearcher.get(2));
-			 counter3++;
-		 }
-		 */
-	 
-	
- 	// It was fileName: "data/entree.txt" if that makes a difference for some reason
- 
- 
-	// Try catch with resoucres, automatically closes br for you in case you forget to include it 
- 	// line.split is where you create an array reading from a file into the size you want it to be. each new index
- 	// is the individual elements based on the comma seperator
- 	// Example: [" grilled chicken:,"vegetable", "Key Lime Pie" ]
-    	
- 
- 
+				counter2++;
+			}
+			fr.close();
+			br.close();
+		}
+		catch (IOException e){
+			System.out.println("The error occurred in readDesserts");
+			e.printStackTrace();
+		}
+		return dessertList;
+
+	}
+
 }
 
--- bch60_MenuManager_v2/src/bch60_MenuManager/MenuTester.java
+++ bch60_MenuManager_v2/src/bch60_MenuManager/MenuTester.java
@@ -1,15 +1,34 @@
 package bch60_MenuManager;
 
+/**
+ * Class MenuTester.java
+ * @author Brandon Hoover
+ * created: 10/25/2022
+ */
+
 public class MenuTester {
 
-		public static void main(String[] args){
-			MenuRandomize randomize = new MenuRandomize("C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\entrees.txt",
-					"C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\sides.txt","C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2"
-							+ "\\data\\salads.txt","C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\desserts.txt");
-			Menu myMenu = randomize.randomMenu();
-			System.out.println(myMenu.description()+"\nTotal calories: "+
-								myMenu.totalCalories());
-		}
+	public static void main(String[] args){
+		MenuRandomize randomize = new MenuRandomize("cd ..\\..\\data\\entrees.txt",
+				"cd ..\\..\\data\\sides.txt","cd ..\\..\\data\\salads.txt","cd ..\\..\\data\\desserts.txt");
+		Menu myMenu = randomize.randomMenu();
+		System.out.println(myMenu.description()+"\nTotal calories: "+
+				myMenu.totalCalories());
 	}
+}
 
-
+/**
+ * The reason that I am using the characters 'cd' in my relative file path is because it was the only way that I could 
+ * get eclipse to read the '..' to back out of the current directory that MenuTester.java is located in, and meneuver
+ * to the current directory where it could access the data and begin to implement the methods that I created. 
+ * 
+ * My absolute file path for the data files on my computer is as follows:
+ * public static String entreeFile="C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\entrees.txt";
+ * public static String sideFile="C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\sides.txt";
+ * public static String saladFile="C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\salads.txt";
+ * public static String dessertFile="C:\\Users\\Brandon\\Documents\\GitHub\\bch60_cmpinf401\\bch60_MenuManager_v2\\data\\desserts.txt";
+ * 
+ * I also wanted to note that the idea for using 'cd' in front of '..' was because of my past experience using linux. 
+ * In linux OS to navigate through terminal you use 'cd .. ' or 'cd the specific file or directory'.
+ * And I am fascinated by the pragmatic programming that allowed for me to find a solution to my problem.
+ */
